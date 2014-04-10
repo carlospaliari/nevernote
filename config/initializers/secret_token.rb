1da9923456f8dd0db3b813169f68a33200413687
carlospalairi <carlos@paliari.com.br>
2014-04-10 14:47:48 -0300

    git inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nevernote::Application.config.secret_key_base = '226b64fb8936173d05a80a023ad3cd37179a8331df6578bf042e626647e4812cd96fe1136a248cdb989c75d3b6fc93a8c3c3f1dc142a0ce7aec19d8073599faf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nevernote::Application.config.session_store :cookie_store, key: '_nevernote_session'
