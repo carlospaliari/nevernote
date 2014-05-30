8a9177ba07521b3ee75e6d26fd01091486cd88f8
carlospalairi <carlos@paliari.com.br>
2014-05-30 15:32:31 -0300

    edit notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,15 @@
 class NotesController < ApplicationController
+  before_action :set_notes, [:index, :edit]
   def index
-    @notes = Note.all
   end
 
   def edit
-    index
+    @note = Note.find params[:id]
     render :index
+  end
+
+  private
+  def set_notes
+    @notes = Note.all
   end
 end
