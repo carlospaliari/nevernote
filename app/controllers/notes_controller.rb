5ea94b570892e0c515652eef66c77e9c8d98dc00
carlospalairi <carlos@paliari.com.br>
2014-05-30 17:15:07 -0300

    salvando as anotacoes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :set_notes, [:index, :edit]
+  before_action :set_notes, [:index, :edit, :save]
   def index
   end
 
@@ -8,8 +8,22 @@ class NotesController < ApplicationController
     render :index
   end
 
+  def save
+    @notes = Note.all
+  end
+
+  def update
+    @note = Note.find params[:id]
+    @note.update(note_params)
+    render :index
+  end
+
   private
   def set_notes
     @notes = Note.all
+  end
+
+  def note_params
+    params.require(:note).permit(:titulo, :texto)
   end
 end
